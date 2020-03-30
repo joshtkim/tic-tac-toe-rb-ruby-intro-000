ed0e94a11881bda3a66b7212efe633621a8fdcf6
Joshua Kim <jykim92@gmail.com>
2020-03-30 20:04:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     player_token = current_player(board)
-    move(board, index)
+    move(board, index, player_token)
     display_board(board)
   else
     turn(board)
